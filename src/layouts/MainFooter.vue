ca694b534d0d23cf7fa01b89f201e0084e23f38a
idsantri <idsantri.mail@gmail.com>
2025-11-01 12:01:47 +0700

    styling footer

--- src/layouts/MainFooter.vue
+++ src/layouts/MainFooter.vue
@@ -1,6 +1,6 @@
 <template>
     <q-footer bordered class="bg-orange-10 text-orange-1">
-        <div class="text-center q-pa-sm">ID Santri</div>
+        <div class="text-center q-pa-xs">ID Santri</div>
     </q-footer>
 </template>
 
